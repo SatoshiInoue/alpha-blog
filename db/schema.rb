3f9fcabb6c308ea5c52d6210d5a274e139775cba
SatoshiInoue <satoshiinoue.biz@gmail.com>
2018-04-22 19:49:09 +0900

    Admin functionality implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_085749) do
+ActiveRecord::Schema.define(version: 2018_04_22_095611) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_04_19_085749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
